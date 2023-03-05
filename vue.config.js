ea433a5ac543ada44ce59c80d005a12b5305d08d
Rdowafun <jabrooster@gmail.com>
2023-03-05 15:24:48 +0500

    fix: change vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
     sourceMap: true
   },
   productionSourceMap: false,
+  publicPath: "/ciniphile/"
 })
